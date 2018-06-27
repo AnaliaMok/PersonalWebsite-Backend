9495db4e0328659a7d945387d84c1857c18392f8
Analia Mok <axm2580@g.rit.edu>
2018-06-26 21:00:18 -0400

    Added dashboard table markup. Implemented default table styling

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,10 +2,21 @@
 
 @section('content')
 <h2>This is the dashboard</h2>
-<ul>
+{{-- <ul>
   @foreach($works as $work)
-    {{-- <li>{{ $work['name'] }}: {{ $work['date'] }}</li> --}}
     <li>{{ $work['title'] }}  {{ $work['date'] }}</li>
   @endforeach
-</ul>
+</ul> --}}
+<div class="table">
+  <ul class="table__row table__row--equal-half table__header">
+    <li class="table__cell table__cell--header">Name</li>
+    <li class="table__cell table__cell--header">Published On</li>
+  </ul>
+  @foreach($works as $work)
+    <ul class="table__row table__row--equal-half">
+      <li class="table__cell">{{ $work['title'] }}</li>
+      <li class="table__cell">@php echo date_format(date_create($work['date']), 'F d, Y'); @endphp</li>
+    </ul>
+  @endforeach
+</div>
 @endsection
